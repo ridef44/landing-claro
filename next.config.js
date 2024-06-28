6ae761d055b11ce1fe9c93c8c9f4f85e7d61af2e
Defry Socop <venturadef@gmail.com>
2024-06-28 15:35:58 -0600

    Import images
    import images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      disableStaticImages: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
